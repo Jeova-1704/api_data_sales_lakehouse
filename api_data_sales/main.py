75e626c522f4080291653ac6baa4a389143f95e4
Jeova BL <jeovab115@gmail.com>
2025-02-14 20:13:48 -0300

    refactor: modificada responsabilidade da rota para procust_router

--- api_data_sales/main.py
+++ api_data_sales/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from api_data_sales.routers.produto_router import router as products_router
 
+app = FastAPI(title='Data lakehouse API', version='1.0.0')
 
-@app.get('/')
-def read_root():
-    return {'message': 'Olá Mundo!'}
+app.include_router(products_router, prefix='/api')
