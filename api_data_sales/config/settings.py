3591ddf00b5794a956d81a4c94802cb286c8a51a
Jeova BL <jeovab115@gmail.com>
2025-02-14 20:14:11 -0300

    refactor: modificada a formatação

--- api_data_sales/config/settings.py
+++ api_data_sales/config/settings.py
@@ -1,13 +1,14 @@
-from supabase import create_async_client
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
+from supabase import create_client
 
-class ConnectionLakehouse():
+
+class ConnectionLakehouse:
     def __init__(self):
         load_dotenv()
-        self.LAKEHOUSE_URL = os.getenv("LAKEHOUSE_URL")
-        self.LAKEHOUSE_KEY = os.getenv("LAKEHOUSE_KEY")
-        
-    async def create_connection(self):
-        return create_async_client(self.LAKEHOUSE_URL, self.LAKEHOUSE_KEY)
+        self.LAKEHOUSE_URL = os.getenv('LAKEHOUSE_URL')
+        self.LAKEHOUSE_KEY = os.getenv('LAKEHOUSE_KEY')
+
+    def create_connection(self):
+        return create_client(self.LAKEHOUSE_URL, self.LAKEHOUSE_KEY)
